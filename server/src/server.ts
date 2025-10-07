1cf48a235fc9f84fd25ab2dbf6232e7790f194c3
Anbu <anbukumar1913@gmail.com>
2025-10-07 17:35:21 +0530

    Final Update

--- server/src/server.ts
+++ server/src/server.ts
@@ -46,7 +46,7 @@ app.use(cors({
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 150,
     message: {
         error: 'Too many requests from this IP, please try again later.'
     },
